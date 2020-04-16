3ff1856c01c1c4bae0f73d593546e9ce5304ad0c
Darko Bozhinovski <dbozhinovski@gmail.com>
2020-04-16 13:47:25 +0200

    Adds editor for services page; Make services page read from markdown

---
+++ content/pages/services.md
@@ -0,0 +1,19 @@
+---
+title: Our services
+type: page
+path: our-services
+---
+
+# Our services
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
+
+<img src="/droidworks170400137.jpg" />
+
+* ### Progressive Web Apps
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
+            
+* ### MVPs & Prototypes
+            
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
--- src/pages/our-services.js
+++ src/pages/our-services.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 import { Layout } from "../components/layout"
 import SEO from "../components/seo"
@@ -7,30 +8,14 @@ import SEO from "../components/seo"
 
 const Services = ({ data, location }) => {
   // const siteTitle = data.site.siteMetadata.title
+  const servicesMdx = data.mdx;
 
   return (
     <Layout>
       <SEO title="Our Services" />
       <div style={{ margin: "20px 0 40px" }}>
-        <h1>Our services</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
-        </p>
-        <img src="/droidworks170400137.jpg" />
-        <ul>
-          <li>
-            <h3>Progressive Web Apps</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
-            </p>
-          </li>
-          <li>
-            <h3>MVPs & Prototypes</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu odio ut sem nulla pharetra. Viverra accumsan in nisl nisi. Sem integer vitae justo eget magna fermentum. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio tempor orci dapibus ultrices in iaculis nunc sed. Leo integer malesuada nunc vel. Ut tristique et egestas quis. Nunc sed id semper risus in. Lectus urna duis convallis convallis tellus.
-            </p>
-          </li>
-        </ul>
+        <h1>{servicesMdx.frontmatter.title}</h1>
+        <MDXRenderer>{servicesMdx.body}</MDXRenderer>
       </div>
     </Layout>
   );
@@ -38,27 +23,20 @@ const Services = ({ data, location }) => {
 
 export default Services;
 
-export const aboutQuery = graphql`
+export const servicesQuery = graphql`
   query {
     site {
       siteMetadata {
         title
       }
     }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
+    mdx(frontmatter: {type: {eq: "page"}, path: {eq: "our-services"}}) {
+      id
+      frontmatter {
+        title
+        type
       }
+      body
     }
   }
 `;
